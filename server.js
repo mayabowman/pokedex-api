31533cb92247d75d650e846981497f0844c66ef5
Maya Bowman <mayashady1@gmail.com>
2019-10-16 07:42:41 -0400

    working through debugging

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-  res.send('Hello, world!');
-});
+console.log(process.env.API_TOKEN);
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.use(function validateBearerToken(req, res, next) {
+  const bearerToken = req.get('Authorization').split(' ')[1];
+  const apiToken = process.env.API_TOKEN;
+  console.log('validate bearer token middleware')
+  if(bearerToken !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next()
+})
+
+function handleGetTypes(req, res) {
+  res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
+  res.send('Hello, Pokemon!')
+}
+
+app.get('/pokemon', handleGetPokemon)
 
 const PORT = 8000;
 
