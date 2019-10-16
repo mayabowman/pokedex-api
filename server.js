1fb8b15badeb712c77aef7e231d8eea64df031d9
Maya Bowman <mayashady1@gmail.com>
2019-10-16 14:05:25 -0400

    worked through exercise and added helmet;

--- server.js
+++ server.js
@@ -1,46 +1,56 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const cors = require('cors');
 const POKEDEX = require('./pokedex.json');
 const app = express();
 app.use(morgan('dev'));
+app.use(helmet());
 app.use(cors());
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
-  console.log('validate bearer token middleware')
-  if(!authToken || authToken.split(' ')[1] !== apiToken) {
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
   }
-  next()
-})
 
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
-
-app.get('/types', function handleGetTypes(req, res) {
-  res.json(validTypes);
+  //move to the next middleware
+  next();
 });
 
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
 app.get('/pokemon', function handleGetPokemon(req, res) {
+
   let response = POKEDEX.pokemon;
 
-  // filter our pokemon by name if name query param is present
-  if(req.query.name) {
-    response = response.filter(pokemon => {
-      // case insensitive searching
-      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase());
-    })
+  //filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      //case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    );
   }
-  // filter our pokemon by type if type query param is present
-  if(req.query.type) {
-    response = response.filter(pokemon => 
+
+  //filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter(pokemon =>
       pokemon.type.includes(req.query.type)
-    )
+    );
   }
-  res.json(response)
-})
+  console.log(req.query.name)
+  console.log(req.query.type)
+  res.json(response);
+});
 
 const PORT = 8000;
 
