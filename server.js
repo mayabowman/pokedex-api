802d5e0f49bdc2c79cf1622e12728456ae6b8d5a
Maya Bowman <mayashady1@gmail.com>
2019-10-16 11:25:23 -0400

    added fetchful and cors

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const POKEDEX = require('./pokedex.json');
 const app = express();
 app.use(morgan('dev'));
-
-console.log(process.env.API_TOKEN);
-
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
-  const bearerToken = req.get('Authorization').split(' ')[1];
   const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
   console.log('validate bearer token middleware')
-  if(bearerToken !== apiToken) {
+  if(!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
   }
   next()
 })
 
-function handleGetTypes(req, res) {
-  res.json(validTypes)
-}
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
-app.get('/types', handleGetTypes)
+app.get('/types', function handleGetTypes(req, res) {
+  res.json(validTypes);
+});
 
-function handleGetPokemon(req, res) {
-  res.send('Hello, Pokemon!')
-}
+app.get('/pokemon', function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
 
-app.get('/pokemon', handleGetPokemon)
+  // filter our pokemon by name if name query param is present
+  if(req.query.name) {
+    response = response.filter(pokemon => {
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase());
+    })
+  }
+  // filter our pokemon by type if type query param is present
+  if(req.query.type) {
+    response = response.filter(pokemon => 
+      pokemon.type.includes(req.query.type)
+    )
+  }
+  res.json(response)
+})
 
 const PORT = 8000;
 
