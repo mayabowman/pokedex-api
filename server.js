bd8d90c5d424f4969d49fb7249eb88cc0deeb03b
Maya Bowman <mayashady1@gmail.com>
2019-10-17 14:59:22 -0400

    implemented heroku and prepared app for deploy

--- server.js
+++ server.js
@@ -1,59 +1,69 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-const POKEDEX = require('./pokedex.json');
-const app = express();
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
+const helmet = require('helmet')
+const cors = require('cors')
+const POKEDEX = require('./pokedex.json')
+const app = express()
+const morganSetting = process.env.NODE_ENV === 'production' ?'tiny' : 'common'
+app.use(morgan(morganSetting))
+app.use(helmet())
+app.use(cors())
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
 app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization');
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
 
-  if (!authToken || authToken.split(" ")[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' });
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
   }
 
   //move to the next middleware
-  next();
-});
+  next()
+})
 
 function handleGetTypes(req, res) {
-  res.json(validTypes);
+  res.json(validTypes)
 }
 
-app.get('/types', handleGetTypes);
+app.get('/types', handleGetTypes)
 
 app.get('/pokemon', function handleGetPokemon(req, res) {
 
-  let response = POKEDEX.pokemon;
+  let response = POKEDEX.pokemon
 
   //filter our pokemon by name if name query param is present
   if (req.query.name) {
     response = response.filter(pokemon =>
       //case insensitive searching
       pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
-    );
+    )
   }
 
   //filter our pokemon by type if type query param is present
   if (req.query.type) {
     response = response.filter(pokemon =>
       pokemon.type.includes(req.query.type)
-    );
+    )
   }
-  console.log(req.query.name)
-  console.log(req.query.type)
-  res.json(response);
-});
+  
+  res.json(response)
+})
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response
+  if(process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server is listening at http://localhost:${PORT}`);
-});
+  console.log(`Server is listening at http://localhost:${PORT}`)
+})
